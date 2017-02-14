249467fc14ab14f25b3dedfb4bdee2417124631b
kitolog <nikita.kosilo@gmail.com>
2017-02-14 19:11:50 +0300

    Updated readme

--- src/appointments.service.ts
+++ src/appointments.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Appointment} from './appointment.model.ts';
+import {Appointment} from './appointment.model';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 import moment from 'moment';
